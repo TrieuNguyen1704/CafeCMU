db173720b56ec08e1ed01b12cf11e1dc46dc0eb8
Danny Nguyen <dathong2901@gmail.com>
2025-11-17 13:30:41 +0700

    thêm bảng cửu chương (test chức năng github)

---
+++ BANG CUU CHUONG.cpp
@@ -0,0 +1,12 @@
+// in bang cuu chuong
+#include <iostream>
+using namespace std;
+main ()
+{
+	for(int i=2 ; i<=9; i++ )
+	{
+	   for(int j=1 ; j<=10 ; j++ )
+	      cout <<i <<"x" <<j <<"=" <<i*j <<endl;
+	      cout <<endl;
+	}
+}
